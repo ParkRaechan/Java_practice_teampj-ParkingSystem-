38b8f49eb910e7453e94f836ca982d545881500d
han0195 <89374809+han0195@users.noreply.github.com>
2022-03-18 21:46:48 +0900

    Add files via upload

--- asd/src/hanyohan/ddd.java
+++ asd/src/hanyohan/ddd.java
@@ -1,5 +1,35 @@
 package hanyohan;
 
-public class ddd {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-}
+public class ddd {
+	
+		
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+			
+		while(true) {
+			try {
+				System.out.println("-------------------- 주차 프로그램 [현황]---------------");
+				System.out.println(/*주차공간 출력*/);
+				System.out.println("----------------------------------------------------");
+				System.out.println("1.주차등록 2.출차 3.매출확인"); int ch = sc.nextInt();
+				
+				if(ch == 1) {
+					
+				}else if(ch == 2) {
+					
+				}else if(ch == 3) {
+					
+				}else {
+					System.out.println("에러)) 알수없는 행동");
+				}
+			}catch(Exception e) {
+				System.out.println("에러)) 관리자 호출 요망" + e);
+			}//예외문 종료
+		}// while문 종료
+		
+	}// me	
+}// cs
